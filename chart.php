6dd03046d9652d7ffb5fc64eb1d6c124c41c2924
Nattaander <bolgeraaron@gmail.com>
2014-09-19 20:51:17 +0100

    Changed App
    Put in questionnaire instead of original with massive files

---
+++ chart.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title>Doughnut Chart</title>
+		<script src="chart/Chart.js"></script>
+		<link rel="stylesheet" href="style/x3dStyle.css">
+		<style>
+			body{ 
+				padding: 0;
+				margin: 0;
+			}
+			#canvas-holder{
+				width:30%;
+			}
+		</style>
+<!-- ............................................................................-->
+		<?php 
+
+					$link = mysqli_connect('192.168.1.4','',''); 
+				if (mysqli_connect_errno()) { 
+					die(' <br> Could not connect to MySQL: ' . mysqli_connect_error()); 
+				} 
+				//else { echo ' <br> Connection OK';
+				//} 
+
+
+				// Select Data Base
+			   if(!mysqli_select_db($link, "test")){
+			   	echo " <br> cant select db";
+			   }
+			  // else {
+			  // 	echo "<br> selected db";
+			  // }
+
+		$v1 = 0;
+		$m1 = 0;
+		$ui1 = 0;
+		$c1 = 0;
+		$i1 = 0;
+		$rows = 0;
+
+		$result = mysqli_query($link,"SELECT * FROM usability");
+		while($row = mysqli_fetch_array($result)) {
+		//echo $row['visual'] . " " . $row['model'] . " " . $row['interface'] . " " . $row['control'] . " " . $row['interact'];
+		$v1 = $v1 + $row['visual'];
+		$m1 = $m1 + $row['model'];
+		$ui1 = $ui1 + $row['interface'];
+		$c1 = $c1 + $row['control'];
+		$i1 = $i1 + $row['interact'];
+		$rows = $rows + 1;
+		}
+
+
+		?>
+<!-- ............................................................................-->
+	</head>
+	<body>
+		<h1>Results</h1>
+		<div id="canvas-holder">
+			<canvas id="chart-area" width="500" height="500"/>
+		</div>
+
+
+	<script>
+
+		var doughnutData = [
+				{
+					value: <?php echo $v1/$rows; ?>,
+					color:"#F7464A",
+					highlight: "#FF5A5E",
+					label: "Average for Visuals"
+				},
+				{
+					value: <?php echo $m1/$rows; ?>,
+					color: "#46BFBD",
+					highlight: "#5AD3D1",
+					label: "Average for Models"
+				},
+				{
+					value: <?php echo $ui1/$rows; ?>,
+					color: "#FDB45C",
+					highlight: "#FFC870",
+					label: "Average for User Interface"
+				},
+				{
+					value: <?php echo $c1/$rows; ?>,
+					color: "#949FB1",
+					highlight: "#A8B3C5",
+					label: "Average for Control"
+				},
+				{
+					value: <?php echo $i1/$rows; ?>,
+					color: "#4D5360",
+					highlight: "#616774",
+					label: "Average for Interactivity"
+				}
+
+			];
+
+			window.onload = function(){
+				var ctx = document.getElementById("chart-area").getContext("2d");
+				window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+			};
+
+
+
+	</script>
+	<div id="btnDiv">
+		<h3>Thank You!</h3>
+		<button id="homeBtn" onclick="location.href='index.html'">Home</button>
+	</div>
+
+	</body>
+	</html>
---
+++ testmysql.php
@@ -0,0 +1,141 @@
+<meta charset=utf-8>
+<?php 
+		//get form stuff and make into variables
+
+		if ($_POST['sex'] == 'male'){
+			$sex = "male";
+	    // Checkbox is selected
+		} else {
+			$sex = "female";
+		   // Alternate code
+		}
+
+		if ($_POST['v'] == '1'){
+			$v = 1;
+		}
+
+		else if ($_POST['v'] == '2'){
+			$v = 2;
+		}
+		else if ($_POST['v'] == '3'){
+			$v = 3;
+		}
+		else if ($_POST['v'] == '4'){
+			$v = 4;
+		}
+		else if ($_POST['v'] == '5'){
+			$v = 5;
+		}
+
+		if ($_POST['m'] == '1'){
+			$m = 1;
+		}
+		else if ($_POST['m'] == '2'){
+			$m = 2;
+		}
+		else if ($_POST['m'] == '3'){
+			$m = 3;
+		}
+		else if ($_POST['m'] == '4'){
+			$m = 4;
+		}
+		else if ($_POST['m'] == '5'){
+			$m = 5;
+		}
+
+		if ($_POST['ui'] == '1'){
+			$ui = 1;
+		}
+		else if ($_POST['ui'] == '2'){
+			$ui = 2;
+		}
+		else if ($_POST['ui'] == '3'){
+			$ui = 3;
+		}
+		else if ($_POST['ui'] == '4'){
+			$ui = 4;
+		}
+		else if ($_POST['ui'] == '5'){
+			$ui = 5;
+		}
+
+
+		if ($_POST['c'] == '1'){
+			$c = 1;
+		}
+		else if ($_POST['c'] == '2'){
+			$c = 2;
+		}
+		else if ($_POST['c'] == '3'){
+			$c = 3;
+		}
+		else if ($_POST['c'] == '4'){
+			$c = 4;
+		}
+		else if ($_POST['c'] == '5'){
+			$c = 5;
+		}
+
+		if ($_POST['i'] == '1'){
+			$i = 1;
+		}
+		else if ($_POST['i'] == '2'){
+			$i = 2;
+		}
+		else if ($_POST['i'] == '3'){
+			$i = 3;
+		}
+		else if ($_POST['i'] == '4'){
+			$i = 4;
+		}
+		else if ($_POST['i'] == '5'){
+			$i = 5;
+		}
+
+		//echo $sex;
+
+
+
+	$link = mysqli_connect('192.168.1.4','',''); 
+	if (mysqli_connect_errno()) { 
+		die(' <br> Could not connect to MySQL: ' . mysqli_connect_error()); 
+	} 
+	//else { echo ' <br> Connection OK';
+	//} 
+
+
+	// Select Data Base
+   if(!mysqli_select_db($link, "test")){
+   	echo " <br> cant select db";
+   }
+   //else {
+   //	echo "<br> selected db";
+  //}
+
+
+   //put into db
+    $sqlAddToDB = "INSERT INTO usability(sex, visual, model, interface, control, interact) VALUES ('$sex', $v, $m, $ui, '$c', '$i')";
+	$insertQuery = mysqli_query($link, $sqlAddToDB);
+
+	if(!$insertQuery){
+		echo "failed to insert". mysql_error();
+	}
+	else {
+		echo "inserted ok";
+	}
+
+	$result = mysqli_query($link,"SELECT * FROM usability");
+
+	//while($row = mysqli_fetch_array($result)) {
+	//	echo "<br>";
+	//  echo $row['sex'] . " " . $row['visual'] . " " . $row['model'] . " " . $row['interface'] . " " . $row['control'] . " " . $row['interact'];
+	//  echo "<br>";
+	//}
+
+
+
+mysqli_close($link); 
+
+//echo file_get_contents("chart.html");
+header("Location: chart.php");
+?> 
